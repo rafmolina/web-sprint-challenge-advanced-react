59bdc8d7ba72f0bd038e469d76e14ff59c182145
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-01-15 11:17:14 -0700

    more testing needed

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -4,6 +4,10 @@ import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const formHead = screen.getByText(/checkout form/i);
+    expect(formHead).toBeInTheDocument();
+});
 
 test("form shows success message on submit with form details", () => {});
